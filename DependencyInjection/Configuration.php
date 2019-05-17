02567238db920d86b650cb72ba5b4d4661918701
Artem Ryzhkov <digital_boy@mail.ru>
2019-05-17 16:10:35 +0700

    fix stricts

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,14 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('smart_core_felib');
+        $treeBuilder = new TreeBuilder('smart_core_felib');
+
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('smart_core_felib');
+        }
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
